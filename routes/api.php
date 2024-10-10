535dbcdf8639bcc1d188bfde5fd1964a07944a17
Владимир Колеснев <vladimirkolesnev@MacBook-Pro.local>
2024-10-11 00:58:15 +0700

    main | changes

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\VpnusersApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->prefix('api')->group(function () {
-    Route::apiResource('vpnusers', VpnusersApiController::class);
-    Route::get('/token', function (Request $request) {
-        $token = csrf_token();
-        return response()->json([
-            'token' => $token,
-        ], 200);
-    });
-    Route::get('/vpnusers/{tg_id}/getLink', [VpnusersApiController::class, 'getLink']);
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('vpnusers', VpnusersApiController::class)->middleware('auth:sanctum');
+Route::get('/vpnusers/{tg_id}/getLink', [VpnusersApiController::class, 'getLink'])->middleware('auth:sanctum');
+
+//Route::get('/token', function (Request $request) {
+//    $token = csrf_token();
+//    return response()->json([
+//        'token' => $token,
+//    ], 200);
+//});
