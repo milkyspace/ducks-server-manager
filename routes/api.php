a16ad3b5ef42eab52869136dc269c3656a2a8289
Владимир Колеснев <vladimirkolesnev@MacBook-Pro.local>
2024-10-11 01:26:55 +0700

    main | changes

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/auth/login', [\App\Http\Controllers\Api\AuthController::class, 'loginUser']);
+
 Route::apiResource('vpnusers', VpnusersApiController::class)->middleware('auth:sanctum');
 Route::get('/vpnusers/{tg_id}/getLink', [VpnusersApiController::class, 'getLink'])->middleware('auth:sanctum');
+Route::post('/tokens/create', function (Request $request) {
+    dd($request->user());
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
 
 //Route::get('/token', function (Request $request) {
 //    $token = csrf_token();
