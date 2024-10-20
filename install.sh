0cfbf230a8664ea3a31a5b52cd1e1a6d1e386f2d
milkyspace <63239588+milkyspace@users.noreply.github.com>
2024-10-20 21:44:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,15 @@ function install() {
     php artisan key:generate
 
     sudo systemctl stop apache2
+    sudo systemctl disable apache2
+    
+    sudo apt -y install ca-certificates curl gnupg lsb-release
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt -y install docker-ce docker-ce-cli containerd.io
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    
     php artisan sail:install
     chmod -R guo+w storage
     php artisan cache:clear
