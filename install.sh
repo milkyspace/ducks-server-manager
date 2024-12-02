e2068baf82f64b3814702ea968f7a1116dda5c41
milkyspace <63239588+milkyspace@users.noreply.github.com>
2024-12-02 20:52:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,6 +37,26 @@ function install() {
     php artisan migrate
     ./vendor/bin/sail up -d
 
+    sudo apt-get install supervisor
+    touch /etc/supervisor/conf.d/laravel-worker.conf
+    mkdir /var/www/ducks-server-manager/logs/
+    touch /var/www/ducks-server-manager/logs/worker.log
+    echo "[program:laravel-worker]
+process_name=%(program_name)s_%(process_num)02d
+command=php /var/www/ducks-server-manager/artisan queue:work --timeout=180
+autostart=true
+autorestart=true
+stopasgroup=true
+killasgroup=true
+;user=forge
+numprocs=8
+redirect_stderr=true
+stdout_logfile=/var/www/ducks-server-manager/logs/worker.log
+stopwaitsecs=3600" >> /etc/supervisor/conf.d/laravel-worker.conf
+    sudo supervisorctl reread
+    sudo supervisorctl update
+    sudo supervisorctl start "laravel-worker:*"
+
     clear
     echo "Installed DUCKS SERVERS MANAGER"
     echo "Congratulations! Now you must configure the .env (copy .env.example to .env) configuration file at .env"
