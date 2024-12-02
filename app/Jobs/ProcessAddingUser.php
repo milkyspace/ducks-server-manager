c278edee0d643583aebdff9f9b32d39ab460eeaf
Владимир Колеснев <vladimirkolesnev@MacBook-Pro.local>
2024-12-02 15:54:11 +0700

    main

--- app/Jobs/ProcessAddingUser.php
+++ app/Jobs/ProcessAddingUser.php
@@ -12,7 +12,9 @@ class ProcessAddingUser implements ShouldQueue
     use Queueable;
 
     public $timeout = 180;
-    
+
+    public $tries = 999;
+
     /**
      * Create a new job instance.
      */
@@ -28,10 +30,22 @@ class ProcessAddingUser implements ShouldQueue
         try {
             $isAdded = $this->server->addUser($this->user);
             if ($isAdded !== true) {
+                $this->fail('Не добавился пользователь ' . $this->user->getUserName() . '(' . $this->user->getId() . ') на сервере ' . $this->server->getServer()->getAddress());
                 $this->release(10);
             }
         } catch (\Exception $e) {
+            $this->fail('Exception Не добавился пользователь ' . $this->user->getUserName() . '(' . $this->user->getId() . ') на сервере ' . $this->server->getServer()->getAddress());
             $this->release(10);
         }
+    }
+
+    /**
+     * Рассчитать количество секунд ожидания перед повторной попыткой выполнения задания.
+     *
+     * @return array<int, int>
+     */
+    public function backoff(): array
+    {
+        return [5, 10, 20];
     }
 }
--- app/Jobs/ProcessUpdatingUser.php
+++ app/Jobs/ProcessUpdatingUser.php
@@ -13,6 +13,8 @@ class ProcessUpdatingUser implements ShouldQueue
 
     public $timeout = 180;
 
+    public $tries = 999;
+
     /**
      * Create a new job instance.
      */
@@ -26,12 +28,24 @@ class ProcessUpdatingUser implements ShouldQueue
     public function handle(): void
     {
         try {
-            $isUpdate = $this->server->updateUser($this->user);;
+            $isUpdate = $this->server->updateUser($this->user);
             if ($isUpdate !== true) {
+                $this->fail('Не обновился пользователь ' . $this->user->getUserName() . '(' . $this->user->getId() . ') на сервере ' . $this->server->getServer()->getAddress());
                 $this->release(10);
             }
         } catch (\Exception $e) {
+            $this->fail('Exception Не обновился пользователь ' . $this->user->getUserName() . '(' . $this->user->getId() . ') на сервере ' . $this->server->getServer()->getAddress());
             $this->release(10);
         }
+    }
+
+    /**
+     * Рассчитать количество секунд ожидания перед повторной попыткой выполнения задания.
+     *
+     * @return array<int, int>
+     */
+    public function backoff(): array
+    {
+        return [5, 10, 20];
     }
 }
